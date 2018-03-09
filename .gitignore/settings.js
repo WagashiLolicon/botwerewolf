7f64c7142ed5bcda7daa3d7f5ffabfc8cdfc40ce
Wagash! Lolicon <wagashilolicon@gmail.com>
2018-03-09 20:03:52 +0100

    Update and rename settings.js to settings.json

--- .gitignore/settings.js
+++
@@ -1,5 +0,0 @@
-	const ownerid = `process.env.ownerid`,
-	const modrolename = "process.env.modrolename",
-	const adminrolename = "process.env.adminrolename",
-	const prefix = "process.env.prefix",
-	const token = `process.env.token`
