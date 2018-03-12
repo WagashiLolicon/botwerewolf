f06eeaec194f904191d603e40fde3e74c60584b6
Wagash! Lolicon <wagashilolicon@gmail.com>
2018-03-12 20:55:39 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const settings = require('./settings.js');
+const settings = require('./settings.json');
 const chalk = require('chalk');
 const fs = require('fs');
 const moment = require('moment');
@@ -95,4 +95,4 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(process.env.token || settings.token);
+client.login(settings.token);
