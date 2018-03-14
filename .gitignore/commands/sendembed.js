014f6d3091bf1c345859668471d49557ea7883e0
Wagash! Lolicon <wagashilolicon@gmail.com>
2018-03-14 19:05:35 +0100

    Create sendembed.js

---
+++ .gitignore/commands/sendembed.js
@@ -0,0 +1,35 @@
+const settings = require('../settings.json');
+exports.run = function(client, message, args) {
+  message.delete(message.author)
+          message.channel.sendMessage("", {
+            embed: {
+                color: 0x837591,
+                title: `Embed edité par ${message.author.username}`,
+                fields: [
+                    {
+                        name: message.content.split(' ')[1],
+                        value: message.content.split(' ').slice(2).join(' '),
+                        inline: true
+                    },
+
+                ],
+                timestamp: new Date(),
+                thumbnail: {
+                    url: message.author.guildURL
+                }
+            }
+        })
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["info", "i"],
+  permLevel: 4
+};
+
+exports.help = {
+  name: 'sendembed',
+  description: 'Envoie un embed edité.',
+  usage: `${settings.prefix}sendembed + titre + texte, exemple: /sendembed One-Word Your Text`
+};
