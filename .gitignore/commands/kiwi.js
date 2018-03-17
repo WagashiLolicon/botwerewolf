1434f9d2e9012f976e7a028d56f2f3bd316fad80
Wagash! Lolicon <wagashilolicon@gmail.com>
2018-03-17 01:02:11 +0100

    Update kiwi.js

--- .gitignore/commands/kiwi.js
+++ .gitignore/commands/kiwi.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
 	function sendCookie(channel, sender, receiver) {
 		if(receiver.bot === true) return message.channel.send(`:kiwi: • **${sender.username}** a donné un kiwi à **<@${receiver.id}>**.. Oh tu es seul ? Désolé pour toi >.<`);
 		else if(receiver.id === sender.id) return message.channel.send(`:kiwi: • Tu aimes tes propres kiwis **<@${sender.id}>** ?`);
-		else return message.channel.send(`:kiwi: • **${sender.username}** a donné un kiwi à**<@${receiver.id}>**`);
+		else return message.channel.send(`:kiwi: • **${sender.username}** a donné un kiwi à **<@${receiver.id}>**`);
 	}
 
 	if(message.mentions.users.first()) return sendCookie(message.channel, message.author, message.mentions.users.first());
