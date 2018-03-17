ed3a7f3ca54fefd5155d55fbd2cec2febb7f9e84
Wagash! Lolicon <wagashilolicon@gmail.com>
2018-03-17 12:48:33 +0100

    Update kiwi.js

--- .gitignore/commands/kiwi.js
+++ .gitignore/commands/kiwi.js
@@ -1,25 +1,25 @@
 const settings = require('../settings.json');
 exports.run = (client, message, args) => {
-	function sendCookie(channel, sender, receiver) {
-		if(receiver.bot === true) return message.channel.send(`:kiwi: • **${sender.username}** a donné un kiwi à **<@${receiver.id}>**.. Oh tu es seul ? Désolé pour toi >.<`);
-		else if(receiver.id === sender.id) return message.channel.send(`:kiwi: • Tu aimes tes propres kiwis **<@${sender.id}>** ?`);
-		else return message.channel.send(`:kiwi: • **${sender.username}** a donné un kiwi à **<@${receiver.id}>**`);
+	function sendKiwi(channel, sender, receiver) {
+		if(receiver.bot === true) return message.channel.send(`:kiwi: ➜ **${sender.username}** a décidé de donnr un kiwi à **<@${receiver.id}>**.. Je suis un bot tu sais ? tu n'as pas d'ami ? ツ >.<`);
+		else if(receiver.id === sender.id) return message.channel.send(`:kiwi: ➜ Pourquoi se donner ses propres kiwis **<@${sender.id}>** ツ ?`);
+		else return message.channel.send(`:kiwi: ➜ **${sender.username}** viens de donner un kiwi à**<@${receiver.id}>** ツ`);
 	}
 
-	if(message.mentions.users.first()) return sendCookie(message.channel, message.author, message.mentions.users.first());
-	else if(args && system.getUser(message, args)) return sendCookie(message.channel, message.author, system.getUser(message, args).user);
-	else return message.channel.send(`Try with : \`/kiwi <user>\``);
+	if(message.mentions.users.first()) return sendKiwi(message.channel, message.author, message.mentions.users.first());
+	else if(args && system.getUser(message, args)) return sendKiwi(message.channel, message.author, system.getUser(message, args).user);
+	else return message.channel.send(`Essaye avec: \`/kiwi <user>\``);
 };
 
 exports.conf = {
 	enabled: true,
 	guildOnly: false,
-	aliases: [""],
-	permLevel: 0,
+	aliases: ["kw"],
+	permLevel: 1,
 };
 
 exports.help = {
 	name: `kiwi`,
-	description: `Give a cookie to someone!`,
+	description: `Donne un kiwi a quelqu'un pour le rendre heureux !`,
 	usage: `${settings.prefix}kiwi <user>`
 };
