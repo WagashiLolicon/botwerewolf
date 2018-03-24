914cf9b8a5988d915141231ec9b005bd3c3d6912
Wagash! Lolicon <wagashilolicon@gmail.com>
2018-03-24 18:22:37 +0100

    Update eventLoader.js

--- .gitignore/util/eventLoader.js
+++ .gitignore/util/eventLoader.js
@@ -3,4 +3,5 @@ module.exports = client => {
   client.on('ready', () => reqEvent('ready')(client));
   client.on('reconnecting', () => reqEvent('reconnecting')(client));
   client.on('disconnect', () => reqEvent('disconnect')(client));
+  client.on('message', reqEvent('message'));
 };
