3bfae868bb78b7f2bbf53bcc8d3dbbda25171129
Wagash! Lolicon <wagashilolicon@gmail.com>
2018-03-24 18:10:44 +0100

    Delete guildMemberAdd.js

--- .gitignore/events/guildMemberAdd.js
+++
@@ -1,7 +0,0 @@
-module.exports = member => {
-let v=guild.channels;
-v.find('name', 'general')?w=v.find('name', 'general'):w=v.filter(c => (c.type === 'text')).first()
-
-w.send(`Le Membre ${member.user.username} a rejoint le serveur dite lui bonjour`)
-};
-
