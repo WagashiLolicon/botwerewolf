4ce741e0e69c45e727cf518201eefaa5e9dcf7fd
Wagash! Lolicon <wagashilolicon@gmail.com>
2018-03-24 18:10:10 +0100

    Delete guildBanAdd.js

--- .gitignore/events/guildBanAdd.js
+++
@@ -1,7 +0,0 @@
-module.exports = (guild, user) => {
-let v=guild.channels;
-let w 
-v.find('name', 'general')?w=v.find('name', 'general'):w=v.filter(c => (c.type === 'text')).first()
-
-w.send(`${member.user.username}`)
-};
